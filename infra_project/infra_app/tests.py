d432798e9b2c71dd97bf2cd57e950d3f97770577
Тимофей Городилов <timotheos94@protonmail.com>
2022-12-21 18:33:18 +0300

    зациклил тесты

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -2,23 +2,26 @@ from http import HTTPStatus
 
 from django.test import Client, TestCase
 
+urls_content = (
+    ('/', 'У меня получилось!')
+    ('/second_page/', 'А это вторая страница')
+)
+
 
 class StaticPagesURLTests(TestCase):
-    def setUp(self):
-        self.guest_client = Client()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.guest_client = Client()
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get('/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-        response = self.guest_client.get('/second_page/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        for url, _ in urls_content:
+            response = self.guest_client.get(url)
+            self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
         """Проверка контента страниц."""
-        response = self.guest_client.get('/')
-        self.assertContains(response, 'У меня получилось!')
-
-        response = self.guest_client.get('/second_page/')
-        self.assertContains(response, 'А это вторая страница!')
+        for url, content in urls_content:
+            response = self.guest_client.get(url)
+            self.assertContains(response, content)
