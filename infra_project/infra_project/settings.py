9ff23b7a330a261ef63a676a889534772490a124
Тимофей Городилов <timotheos94@protonmail.com>
2022-12-21 19:54:37 +0300

    settings patched

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,19 +25,24 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    'testserver'
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'infra_app.apps.InfraAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'infra_app.apps.InfraAppConfig',
 ]
 
 MIDDLEWARE = [
